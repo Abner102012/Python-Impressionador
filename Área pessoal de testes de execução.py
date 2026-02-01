8554b5e3ec7a68a977b12b9f8031c3fe55126254
Abner <abner.gui.soares@gmail.com>
2026-02-01 16:00:59 -0300

    :)

--- Área pessoal de testes de execução.py
+++ Área pessoal de testes de execução.py
@@ -1,27 +1,12 @@
-meses = [
-    'Janeiro',
-    'Fevereiro',
-    'Março',
-    'Abril',
-    'Maio',
-    'Junho',
-    'Julho',
-    'Agosto',
-    'Setembro',
-    'Outubro',
-    'Novembro',
-    'Dezembro'
+produtos = [
+    ("Mouse", 15),
+    ("Teclado", 30),
+    ("Monitor", 150),
+    ("Headset", 12)
 ]
 
-temperaturas = [30, 29, 28, 28, 25, 26, 20, 21, 19, 25, 27, 32]
-
-
-for i, mes in enumerate(meses):
-    print(f"Mês: {mes}. Temperatura: {temperaturas[i]}°C")
-
-maior_temp = max(temperaturas)
-indice_mais_quente = temperaturas.index(maior_temp)
-mes_mais_quente = meses[indice_mais_quente]
-print(f"O mês mais quente é {mes_mais_quente} com {maior_temp}°C.")
-
-    
+for produto, preco_dollar in produtos:
+    preco_real = preco_dollar*5.26
+    if preco_real > 100:
+        print(f"{produto} custa R${preco_real:,.2f}")
+        print(f"Já em dollar, o produto {produto} custa ${preco_dollar}")
