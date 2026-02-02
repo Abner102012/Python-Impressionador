e63e448f7dc8ebd0b4ad9d253321beb635b30824
Abner <abner.gui.soares@gmail.com>
2026-02-02 10:50:48 -0300

    bom dia

--- Área pessoal de testes de execução.py
+++ Área pessoal de testes de execução.py
@@ -1,12 +1,9 @@
-produtos = [
-    ("Mouse", 15),
-    ("Teclado", 30),
-    ("Monitor", 150),
-    ("Headset", 12)
-]
+vendas_tecnologia = {'iphone': 15000, 'samsung galaxy': 12000, 'tv samsung': 10000, 'ps5': 14300, 'tablet': 1720, 'ipad': 1000, 'tv philco': 2500, 'notebook hp': 1000, 'notebook dell': 17000, 'notebook asus': 2450}
 
-for produto, preco_dollar in produtos:
-    preco_real = preco_dollar*5.26
-    if preco_real > 100:
-        print(f"{produto} custa R${preco_real:,.2f}")
-        print(f"Já em dollar, o produto {produto} custa ${preco_dollar}")
+total_notebooks = 0
+for chave in vendas_tecnologia:
+    if "notebook" in chave:
+        total_notebooks += vendas_tecnologia[chave]
+    else:
+        continue
+print(total_notebooks)
