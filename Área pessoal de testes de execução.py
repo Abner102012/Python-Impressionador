c3c7b100d398d64f412e0cb0fa6e083a22d267ae
Abner <abner.gui.soares@gmail.com>
2026-02-12 10:49:17 -0300

    :0

--- Área pessoal de testes de execução.py
+++ Área pessoal de testes de execução.py
@@ -1,7 +1,29 @@
-estoque = [('BSA2199',396),('PPF5239',251),('BSA1212',989),('PPF2154',449),('BEB3410',241),('PPF8999',527),('EMB9591',601),('BSA2006',314),('EMB3604',469),('EMB2070',733),('PPF9018',339),('PPF1468',906),('BSA5819',291),('PPF8666',850),('BEB2983',353),('BEB5877',456),('PPF5008',963),('PPF3877',185),('PPF7321',163),('BSA8833',644),('PPF4980',421),('PPF3063',757),('BSA2089',271),('BSA8398',180),('EMB4622',515),('EMB9814',563),('PPF3784',229),('PPF2398',270),('BEB3211',181),('PPF8655',459),('PPF1874',799),('PPF8789',126),('PPF6324',375),('EMB9290',883),('BSA5516',555),('BSA8451',243),('BSA8213',423)]
 
-estoque_pedido_1000 = [qtd if 200 < qtd < 1000 else 0 for codigo, qtd in estoque]
-estoque_pedido_200 = [qtd if qtd < 200 else 0 for codigo, qtd in estoque]
+lista_compras = {}
 
-print(estoque_pedido_1000)
-print(estoque_pedido_200)
+while True:
+    print()                                     
+    print("1 Adicionar item")
+    print("2 Remover item")
+    print("3 Ver lista")
+    print("4 Sair")
+    
+    escolha = input("Escolha uma opção: ")
+    escolha.strip()
+    escolha.casefold()
+    escolha.capitalize()
+    if escolha == '1':
+        item = input("Digite um item: ")
+        valor = input("Digite o valor: ")
+        lista_compras = [item, valor]
+    elif escolha == '2':
+        item = input("Digite um item: ")
+        valor = input("Digite o valor: ")
+        if item in lista_compras:
+            del item, valor
+    elif escolha == '3':
+        print(lista_compras)
+    elif escolha == '4':
+        break
+    else:
+        print("Coloque os dígitos de 1 a 4")
