5caea117ee97b51fdc7fed2390d231a113eb1902
Abner <abner.gui.soares@gmail.com>
2026-02-16 10:05:28 -0300

    Bom dia

--- Área pessoal de testes de execução.py
+++ Área pessoal de testes de execução.py
@@ -1,29 +1,10 @@
+import time
+import locale
 
-lista_compras = {}
+locale.setlocale(locale.LC_TIME, "pt-BR")
 
-while True:
-    print()                                     
-    print("1 Adicionar item")
-    print("2 Remover item")
-    print("3 Ver lista")
-    print("4 Sair")
-    
-    escolha = input("Escolha uma opção: ")
-    escolha.strip()
-    escolha.casefold()
-    escolha.capitalize()
-    if escolha == '1':
-        item = input("Digite um item: ")
-        valor = input("Digite o valor: ")
-        lista_compras = [item, valor]
-    elif escolha == '2':
-        item = input("Digite um item: ")
-        valor = input("Digite o valor: ")
-        if item in lista_compras:
-            del item, valor
-    elif escolha == '3':
-        print(lista_compras)
-    elif escolha == '4':
-        break
-    else:
-        print("Coloque os dígitos de 1 a 4")
+data_atual = time.localtime()
+
+data_texto  = time.strftime("%A, %d de %B de %Y, %H:%M", data_atual)
+
+print(data_texto.title())
